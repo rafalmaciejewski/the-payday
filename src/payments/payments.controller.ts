ddf61b3e73d60fc69ea64b551874df94a0b22957
Rafał Maciejewski <rafaljestsuper@gmail.com>
2022-03-31 00:07:10 +0200

    feat: add endpoint for csv file download

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Header, Query, Res } from '@nestjs/common';
+import { ApiHeader, ApiHeaders, ApiParam, ApiProduces, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
 import { PaymentsService } from './payments.service';
 import { ParseDatePipe } from './pipes/parse-date.pipe';
 
@@ -9,8 +10,31 @@ export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
   @Get()
+  @Header('Content-Type', 'application/json')
+  @ApiParam({
+    name: 'date',
+    description: 'Date to get payments for',
+    type: 'string',
+    format: 'date',
+    required: false,
+  })
   getPayments(@Query('date', ParseDatePipe) date: Date): string[] {
     return this.getPaymentDates(date);
+  }
+
+  @Get('/csv')
+  @Header('Content-Type', 'text/csv')
+  @ApiParam({
+    name: 'date',
+    description: 'Date to get payments for',
+    type: 'string',
+    format: 'date',
+    required: false,
+  })
+  getPaymentsCSV(@Query('date', ParseDatePipe) date: Date, @Res() res: Response): void {
+    const paymentDates = this.getPaymentDates(date);
+    res.attachment('payments.csv');
+    res.send(paymentDates.join('\n'));
   }
 
   private getPaymentDates(date: Date = new Date()): string[] {
