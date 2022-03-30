603204b27530b6add9b2dc79cb3a078cf3ba09e9
Rafał Maciejewski <rafaljestsuper@gmail.com>
2022-03-30 23:50:20 +0200

    feat: get 12-month payment plan for given date

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
 
+const MONTHS = 12;
+
 @Injectable()
 export class PaymentsService {
   getPaymentDates(date: Date): Date[] {
-    return [date];
+    const result = [];
+    for (let i = 0; i < MONTHS; i++) {
+      const paymentDate = new Date(date.getFullYear(), date.getMonth() + i, i > 0 ? 1 : date.getDate());
+      result.push(this.getBasePaymentDate(paymentDate));
+    }
+    return result.filter(Boolean);
+  }
+
+  private isBusinessDay(date: Date): boolean {
+    const weekday = date.getUTCDay();
+    const SATURDAY = 6;
+    const SUNDAY = 0;
+    return weekday !== SUNDAY && weekday !== SATURDAY;
+  }
+
+  private getBasePaymentDate(date: Date): Date {
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+    while (!this.isBusinessDay(lastDay)) {
+      lastDay.setDate(lastDay.getDate() - 1);
+    }
+    return lastDay.getTime() > date.getTime() ? lastDay : null;
   }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { StatusCodes } from 'http-status-codes';
+import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 
 describe('The Boroughs Test', () => {
@@ -14,7 +16,27 @@ describe('The Boroughs Test', () => {
     await app.init();
   });
 
-  it('should work', () => {
-    expect(true).not.toBe(true);
+  describe('payments', () => {
+    it('should return payment dates for next 12 months from given dane', () =>
+      request(app.getHttpServer())
+        .get('/payments')
+        .query({ date: '2022-03-01' })
+        .expect(StatusCodes.OK)
+        .expect((res) => {
+          expect(res.body).toEqual([
+            '2022-03-31',
+            '2022-04-29',
+            '2022-05-31',
+            '2022-06-30',
+            '2022-07-29',
+            '2022-08-31',
+            '2022-09-30',
+            '2022-10-31',
+            '2022-11-30',
+            '2022-12-30',
+            '2023-01-31',
+            '2023-02-28',
+          ]);
+        }));
   });
 });
